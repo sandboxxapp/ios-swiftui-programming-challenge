341a1b436bef54d992df2aad713a116919973b79
hbanzon <hino.banzon@gmail.com>
2022-02-03 14:16:52 -0500

    Initial Commit

---
+++ swapi-swiftui/swapi_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swapi_swiftuiApp.swift
+//  swapi-swiftui
+//
+//  Created by Hino Banzon on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct swapi_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
